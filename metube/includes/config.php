9c75b0f6482ca1fabf57a53da3a6f9fe5cd50546
X O <xo@wvvw.me>
2015-12-06 00:48:08 +1030

    add metube

---
+++ metube/includes/config.php
@@ -0,0 +1,4 @@
+<?php
+$site_url = 'http://metube.com/';
+date_default_timezone_set('Australia/Adelaide');
+?>
---
+++ metube/includes/header.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include_once 'includes/connect.php';
+include_once 'includes/config.php';
+include_once 'includes/functions.php'
+?>
+<html>
+    <head>
+        <title>MeTube</title>
+        <link rel="stylesheet" type="text/css" href="/includes/style.css" media="screen"/>
+    </head>
+    <body>
+    <div id="wrapper">
+        <div id="header">
+        </div>
+        <div id="content">
+            
