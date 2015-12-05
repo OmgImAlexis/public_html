d1ea98676096adfe6fbcf8e27519c31a077a247a
X O <xo@wvvw.me>
2015-12-06 01:05:27 +1030

    add pt.klayfx.com

---
+++ pt.klayfx.com/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Personal Tools</title>
+        <link rel="stylesheet" type="text/css" href="includes/style.css" media="screen"/>
+    </head>
+    <body>
+        <h1>Personal Tools by KlayFX</h1>
+        <h3>1.0 inital release - 28th October 2012</h3>
+        <li>Basic sent, received and summary pages</li>
+        <li>Frame for updates page</li>
+        {@code &lt;application android:theme="@android:style/Theme.Holo"&gt;}
+        <li>Email and website links</li>
+    </body>
+</html>
